0bcb760929f3189bf3f9a52cc2542ac05d68f420
szymon <szymon.pt4sznik@gmail.com>
2026-04-18 15:38:45 +0200

    added post request to ue url

--- resources/epc_requests.py
+++ resources/epc_requests.py
@@ -14,8 +14,16 @@ def get_ue_by_id(url, id):
         return data
     return
 
+def attach_ue(url, id):
+    payload = {"ue_id": id}
+    resp = requests.post(url, json=payload)
+    if resp.status_code == 200:
+        data = resp.json()
+        return True
+    return False
+
 if __name__ == '__main__':
-    ues_url = 'http://10.0.40.5:8000/ues'
-    signle_ue_url = 'http://10.0.40.5:8000/ues'
-    print(get_ues(ues_url))
-    print(get_ue_by_id(signle_ue_url, 1))
+    url = 'http://10.0.40.5:8000/ues'
+    print(get_ues(url))
+    print(get_ue_by_id(url, 1))
+    # print(attach_ue(url, 4))
