ecb4b527c8387a8bf684365231fc9683c43590c8
szymon <szymon.pt4sznik@gmail.com>
2026-04-18 15:26:46 +0200

    added function for requesting list of ues and single ue

--- resources/epc_requests.py
+++ resources/epc_requests.py
@@ -1,4 +1,21 @@
 import requests
 
+def get_ues(url):
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        data = resp.json()
+        return data['ues']
+    return
+
+def get_ue_by_id(url, id):
+    resp = requests.get(f'{url}/{id}')
+    if resp.status_code == 200:
+        data = resp.json()
+        return data
+    return
+
 if __name__ == '__main__':
-    print('epc_requests.py')
+    ues_url = 'http://10.0.40.5:8000/ues'
+    signle_ue_url = 'http://10.0.40.5:8000/ues'
+    print(get_ues(ues_url))
+    print(get_ue_by_id(signle_ue_url, 1))
