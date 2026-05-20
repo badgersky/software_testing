b18cbb16495a0eeb2bd1c0b701da6914fee0def6
szymon <szymon.pt4sznik@gmail.com>
2026-05-20 22:50:28 +0200

    added ues_stats and detach_ue requests methods, added prints to see detailed methods responses

--- resources/epc_requests.py
+++ resources/epc_requests.py
@@ -8,23 +8,47 @@ class epc_requests:
 
     def get_ues(self):
         resp = requests.get(self.base_url)
-        if resp.status_code == 200:
-            return resp.json()['ues']
-        return
+        return resp.json()
 
     def get_ue(self, id):
         resp = requests.get(f'{self.base_url}/{id}')
-        if resp.status_code == 200:
-            return resp.json()
-        return
+        return resp.json()
 
     def attach_ue(self, id):
-        payload = {"ue_id": id}
+        payload = {'ue_id': id}
         resp = requests.post(self.base_url, json=payload)
-        if resp.status_code == 200:
-            return True
-        return False
+        return resp.json()
 
     def reset_simulator(self):
         resp = requests.post(self.base_url.replace('/ues', '/reset'))
         return resp.status_code == 200
+    
+    def get_ues_stats(self):
+        resp = requests.get(f'{self.base_url}/stats')
+        return resp.json()
+    
+    def detach_ue(self, id):
+        payload = {'ue_id': id}
+        resp = requests.delete(f'{self.base_url}/{id}', json=payload)
+        return resp.json()
+    
+
+if __name__ == '__main__':
+    req = epc_requests('http://localhost:8000/ues')
+    print('attach_ue:')
+    print(req.attach_ue(2))
+    print(req.attach_ue(3))
+    print(req.attach_ue(5))
+    print(req.attach_ue(150))
+    print('get_ue:')
+    print(req.get_ue(2))
+    print(req.get_ue(3))
+    print(req.get_ue(150))
+    print('get_ues:')
+    print(req.get_ues())
+    print('get_ues_stats:')
+    print(req.get_ues_stats())
+    print('detach_ue:')
+    print(req.detach_ue(2))
+    print(req.detach_ue(14))
+    req.reset_simulator()
